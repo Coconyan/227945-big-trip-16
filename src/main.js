e70b13b8b5baa8c0bbddb3983d142db5c7675db7
Coconyan <San3k19@gmail.com>
2021-12-01 20:49:52 +0700

    add render function, add filters-view class

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { renderTemplate, RenderPosition } from './render.js';
+import { renderTemplate, RenderPosition, render } from './render.js';
 import { createMenuTemplate } from './view/menu-view.js';
-import { createFiltersTemplate } from './view/filters-view.js';
+import SiteFilterView from './view/filters-view.js';
 import { createSortTemplate } from './view/sort-view.js';
 import { createFormCreateTemplate } from './view/form-create-view.js';
 import { createFormEditTemplate } from './view/form-edit-view.js';
@@ -16,7 +16,7 @@ const tripControlsElement = document.querySelector('.trip-controls__navigation')
 const tripFiltersElement = document.querySelector('.trip-controls__filters');
 
 renderTemplate(tripControlsElement, createMenuTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(tripFiltersElement, createFiltersTemplate(), RenderPosition.BEFOREEND);
+render(tripFiltersElement, new SiteFilterView().element, RenderPosition.BEFOREEND);
 
 const tripEventsElement = document.querySelector('.trip-events');
 
