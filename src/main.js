970ff3021d2eb00ae030b5fc014fa2837cc9365f
Coconyan <San3k19@gmail.com>
2021-12-01 21:10:21 +0700

    edit simple components to classes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { renderTemplate, RenderPosition, render } from './render.js';
-import { createMenuTemplate } from './view/menu-view.js';
-import SiteFilterView from './view/filters-view.js';
-import { createSortTemplate } from './view/sort-view.js';
-import { createFormCreateTemplate } from './view/form-create-view.js';
+import MenuView from './view/menu-view.js';
+import FilterView from './view/filters-view.js';
+import SortView from './view/sort-view.js';
+import FormCreateView from './view/form-create-view.js';
+import ListElementView from './view/list-view.js';
 import { createFormEditTemplate } from './view/form-edit-view.js';
 import { createListItemTemplate } from './view/list-item-view.js';
-import { createListElement } from './view/list-view.js';
 import { generatePoint } from './mock/point.js';
 import { createTripInfoTemplate } from './view/header-trip-info-view.js';
 
@@ -15,18 +15,18 @@ const points = Array.from({length: ITEMS_COUNT}, generatePoint);
 const tripControlsElement = document.querySelector('.trip-controls__navigation');
 const tripFiltersElement = document.querySelector('.trip-controls__filters');
 
-renderTemplate(tripControlsElement, createMenuTemplate(), RenderPosition.BEFOREEND);
-render(tripFiltersElement, new SiteFilterView().element, RenderPosition.BEFOREEND);
+render(tripControlsElement, new MenuView().element, RenderPosition.BEFOREEND);
+render(tripFiltersElement, new FilterView().element, RenderPosition.BEFOREEND);
 
 const tripEventsElement = document.querySelector('.trip-events');
 
-renderTemplate(tripEventsElement, createListElement(), RenderPosition.BEFOREEND);
+render(tripEventsElement, new ListElementView().element, RenderPosition.BEFOREEND);
 
 const tripListElement = tripEventsElement.querySelector('.trip-events__list');
 
-renderTemplate(tripListElement, createSortTemplate(), RenderPosition.BEFOREEND);
+render(tripListElement, new SortView().element, RenderPosition.BEFOREEND);
 renderTemplate(tripListElement, createFormEditTemplate(points[0]), RenderPosition.BEFOREEND);
-renderTemplate(tripListElement, createFormCreateTemplate(), RenderPosition.BEFOREEND);
+render(tripListElement, new FormCreateView().element, RenderPosition.BEFOREEND);
 
 for (let i = 1; i < ITEMS_COUNT; i++) {
   renderTemplate(tripListElement, createListItemTemplate(points[i]), RenderPosition.BEFOREEND);
